4beaee02334841d0ecb899fb6c3b548f0834a451
Igor_Sysoev <Igor_Sysoev@epam.com>
2017-12-25 19:29:07 +0300

    routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Route} from 'react-router'
+import {BrowserRouter, Switch} from 'react-router-dom'
+import Template from './containers/Template'
+import Profile from "./containers/Profile";
+import Home from "./containers/Home";
+
 
 ReactDOM.render(
-  <div>
-    <h1>Hello Igor!!!</h1>
-  </div>,
+  <BrowserRouter>
+    <Route path='/' component={Template}>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/profile' component={Profile}/>
+      </Switch>
+    </Route>
+  </BrowserRouter>,
   document.getElementById('root'))
 
