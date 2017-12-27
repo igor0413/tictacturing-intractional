5f8b92252b44c6250ec0b81648f8769197a8216b
Igor_Sysoev <Igor_Sysoev@epam.com>
2017-12-27 18:44:08 +0300

    game functionality

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import {Route} from 'react-router'
 import {BrowserRouter, Switch} from 'react-router-dom'
 import Template from './containers/Template'
 import Profile from "./containers/Profile";
-import Home from "./containers/Home";
+import TicTacToe from "./containers/TicTacToe";
 
 
 ReactDOM.render(
   <BrowserRouter>
     <Template>
       <Switch>
-        <Route exact path='/' component={Home}/>
+        <Route exact path='/' component={TicTacToe}/>
         <Route path='/profile' component={Profile}/>
       </Switch>
     </Template>
